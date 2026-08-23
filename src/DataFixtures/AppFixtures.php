502fd2a05f36661cac9f273256e50615b6033306
Romane <romane@macbookair.home>
2026-08-22 20:12:31 -0400

    Add fixture users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,168 +2,271 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Theme;
 use App\Entity\Course;
 use App\Entity\Lesson;
+use App\Entity\Theme;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-/** Loads initial data: themes, courses and lessons. */
+/**
+ * Loads initial data: users, themes, courses and lessons.
+ */
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher
+    ) {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        //Music theme and courses
+        /*
+         * ============================================================
+         * USERS
+         * ============================================================
+         */
+
+        // Admin
+        $admin = new User();
+        $admin->setEmail('admin@admin.com');
+        $admin->setRoles(['ROLE_ADMIN', 'ROLE_CLIENT']);
+        $admin->setIsVerified(true);
+        $admin->setPassword(
+            $this->passwordHasher->hashPassword($admin, 'admintest')
+        );
+
+        $manager->persist($admin);
+
+        // Client 1
+        $user = new User();
+        $user->setEmail('user@user.com');
+        $user->setRoles(['ROLE_CLIENT']);
+        $user->setIsVerified(true);
+        $user->setPassword(
+            $this->passwordHasher->hashPassword($user, 'useruser')
+        );
+
+        $manager->persist($user);
+
+        // Client 2
+        $user2 = new User();
+        $user2->setEmail('useruser@user.com');
+        $user2->setRoles(['ROLE_CLIENT']);
+        $user2->setIsVerified(true);
+        $user2->setPassword(
+            $this->passwordHasher->hashPassword($user2, 'User-2l')
+        );
+
+        $manager->persist($user2);
+
+        /*
+         * ============================================================
+         * MUSIQUE
+         * ============================================================
+         */
+
         $music = new Theme();
         $music->setTitle('Musique');
+
         $manager->persist($music);
 
+        // Guitare
         $guitar = new Course();
         $guitar->setTitle("Cursus d'initiation à la guitare");
         $guitar->setPrice(50);
         $guitar->setTheme($music);
+
         $manager->persist($guitar);
 
-        $lesson1 = new Lesson();
-        $lesson1->setTitle("Découverte de l'instrument");
-        $lesson1->setPrice(26);
-        $lesson1->setCourse($guitar);
-        $lesson1->setContent('Lorem ipsum...');
-        $lesson1->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
-        $manager->persist($lesson1);
-        
-        $lesson2 = new Lesson();
-        $lesson2->setTitle("Les accords et les gammes");
-        $lesson2->setPrice(26);
-        $lesson2->setCourse($guitar);
-        $lesson2->setContent('Lorem ipsum...');
-        $lesson2->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
-        $manager->persist($lesson2);
+        $lesson = new Lesson();
+        $lesson->setTitle("Découverte de l'instrument");
+        $lesson->setPrice(26);
+        $lesson->setCourse($guitar);
+        $lesson->setContent('Lorem ipsum...');
+        $lesson->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
 
+        $manager->persist($lesson);
+
+        $lesson = new Lesson();
+        $lesson->setTitle("Les accords et les gammes");
+        $lesson->setPrice(26);
+        $lesson->setCourse($guitar);
+        $lesson->setContent('Lorem ipsum...');
+        $lesson->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
+
+        $manager->persist($lesson);
+
+        // Piano
         $piano = new Course();
         $piano->setTitle("Cursus d'initiation au piano");
         $piano->setPrice(50);
         $piano->setTheme($music);
+
         $manager->persist($piano);
 
-        $lesson1 = new Lesson();
-        $lesson1->setTitle("Découverte de l'instrument");
-        $lesson1->setPrice(26);
-        $lesson1->setCourse($piano);
-        $lesson1->setContent('Lorem ipsum...');
-        $lesson1->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
-        $manager->persist($lesson1);
-        
-        $lesson2 = new Lesson();
-        $lesson2->setTitle("Les accords et les gammes");
-        $lesson2->setPrice(26);
-        $lesson2->setCourse($piano);
-        $lesson2->setContent('Lorem ipsum...');
-        $lesson2->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
-        $manager->persist($lesson2);
-        
-        //IT theme and courses
+        $lesson = new Lesson();
+        $lesson->setTitle("Découverte de l'instrument");
+        $lesson->setPrice(26);
+        $lesson->setCourse($piano);
+        $lesson->setContent('Lorem ipsum...');
+        $lesson->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
+
+        $manager->persist($lesson);
+
+        $lesson = new Lesson();
+        $lesson->setTitle("Les accords et les gammes");
+        $lesson->setPrice(26);
+        $lesson->setCourse($piano);
+        $lesson->setContent('Lorem ipsum...');
+        $lesson->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
+
+        $manager->persist($lesson);
+
+        /*
+         * ============================================================
+         * INFORMATIQUE
+         * ============================================================
+         */
+
         $it = new Theme();
         $it->setTitle('Informatique');
+
         $manager->persist($it);
 
+        // Développement web
         $web = new Course();
         $web->setTitle("Cursus d’initiation au développement web");
         $web->setPrice(60);
         $web->setTheme($it);
+
         $manager->persist($web);
 
-        $lesson1 = new Lesson();
-        $lesson1->setTitle("Les langages Html et CSS");
-        $lesson1->setPrice(32);
-        $lesson1->setCourse($web);
-        $lesson1->setContent('Lorem ipsum...');
-        $lesson1->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
-        $manager->persist($lesson1);
+        $lesson = new Lesson();
+        $lesson->setTitle("Les langages Html et CSS");
+        $lesson->setPrice(32);
+        $lesson->setCourse($web);
+        $lesson->setContent('Lorem ipsum...');
+        $lesson->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
 
-        $lesson2 = new Lesson();
-        $lesson2->setTitle("Dynamiser votre site avec Javascript");
-        $lesson2->setPrice(32);
-        $lesson2->setCourse($web);
-        $lesson2->setContent('Lorem ipsum...');
-        $lesson2->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
-        $manager->persist($lesson2);
-        
-        //Gardening theme and courses
+        $manager->persist($lesson);
+
+        $lesson = new Lesson();
+        $lesson->setTitle("Dynamiser votre site avec Javascript");
+        $lesson->setPrice(32);
+        $lesson->setCourse($web);
+        $lesson->setContent('Lorem ipsum...');
+        $lesson->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
+
+        $manager->persist($lesson);
+
+        /*
+         * ============================================================
+         * JARDINAGE
+         * ============================================================
+         */
+
         $gardening = new Theme();
         $gardening->setTitle('Jardinage');
+
         $manager->persist($gardening);
 
         $garden = new Course();
         $garden->setTitle("Cursus d’initiation au jardinage");
         $garden->setPrice(30);
         $garden->setTheme($gardening);
+
         $manager->persist($garden);
 
-        $lesson1 = new Lesson();
-        $lesson1->setTitle("Les outils du jardinier");
-        $lesson1->setPrice(16);
-        $lesson1->setCourse($garden);
-        $lesson1->setContent('Lorem ipsum...');
-        $lesson1->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
-        $manager->persist($lesson1);
-        
-        $lesson2 = new Lesson();
-        $lesson2->setTitle("Jardiner avec la lune");
-        $lesson2->setPrice(16);
-        $lesson2->setCourse($garden);
-        $lesson2->setContent('Lorem ipsum...');
-        $lesson2->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
-        $manager->persist($lesson2);
-        
-        //Cooking theme and courses
+        $lesson = new Lesson();
+        $lesson->setTitle("Les outils du jardinier");
+        $lesson->setPrice(16);
+        $lesson->setCourse($garden);
+        $lesson->setContent('Lorem ipsum...');
+        $lesson->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
+
+        $manager->persist($lesson);
+
+        $lesson = new Lesson();
+        $lesson->setTitle("Jardiner avec la lune");
+        $lesson->setPrice(16);
+        $lesson->setCourse($garden);
+        $lesson->setContent('Lorem ipsum...');
+        $lesson->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
+
+        $manager->persist($lesson);
+
+        /*
+         * ============================================================
+         * CUISINE
+         * ============================================================
+         */
+
         $cooking = new Theme();
         $cooking->setTitle('Cuisine');
+
         $manager->persist($cooking);
 
+        // Cuisine
         $cook = new Course();
         $cook->setTitle("Cursus d’initiation à la cuisine");
         $cook->setPrice(44);
         $cook->setTheme($cooking);
+
         $manager->persist($cook);
 
-        $lesson1 = new Lesson();
-        $lesson1->setTitle("Les modes de cuisson");
-        $lesson1->setPrice(23);
-        $lesson1->setCourse($cook);
-        $lesson1->setContent('Lorem ipsum...');
-        $lesson1->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
-        $manager->persist($lesson1);
-        
-        $lesson2 = new Lesson();
-        $lesson2->setTitle("Les saveurs");
-        $lesson2->setPrice(23);
-        $lesson2->setCourse($cook);
-        $lesson2->setContent('Lorem ipsum...');
-        $lesson2->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
-        $manager->persist($lesson2);
+        $lesson = new Lesson();
+        $lesson->setTitle("Les modes de cuisson");
+        $lesson->setPrice(23);
+        $lesson->setCourse($cook);
+        $lesson->setContent('Lorem ipsum...');
+        $lesson->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
 
+        $manager->persist($lesson);
+
+        $lesson = new Lesson();
+        $lesson->setTitle("Les saveurs");
+        $lesson->setPrice(23);
+        $lesson->setCourse($cook);
+        $lesson->setContent('Lorem ipsum...');
+        $lesson->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
+
+        $manager->persist($lesson);
+
+        // Art du dressage culinaire
         $presentation = new Course();
-        $presentation->setTitle("Cursus d’initiation à l’art du dressage culinaire");
+        $presentation->setTitle(
+            "Cursus d’initiation à l’art du dressage culinaire"
+        );
         $presentation->setPrice(48);
         $presentation->setTheme($cooking);
+
         $manager->persist($presentation);
 
-        $lesson1 = new Lesson();
-        $lesson1->setTitle("Mettre en œuvre le style dans l’assiette");
-        $lesson1->setPrice(26);
-        $lesson1->setCourse($presentation);
-        $lesson1->setContent('Lorem ipsum...');
-        $lesson1->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
-        $manager->persist($lesson1);
-        
-        $lesson2 = new Lesson();
-        $lesson2->setTitle("Harmoniser un repas à quatre plats");
-        $lesson2->setPrice(26);
-        $lesson2->setCourse($presentation);
-        $lesson2->setContent('Lorem ipsum...');
-        $lesson2->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
-        $manager->persist($lesson2);
+        $lesson = new Lesson();
+        $lesson->setTitle("Mettre en œuvre le style dans l’assiette");
+        $lesson->setPrice(26);
+        $lesson->setCourse($presentation);
+        $lesson->setContent('Lorem ipsum...');
+        $lesson->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
+
+        $manager->persist($lesson);
+
+        $lesson = new Lesson();
+        $lesson->setTitle("Harmoniser un repas à quatre plats");
+        $lesson->setPrice(26);
+        $lesson->setCourse($presentation);
+        $lesson->setContent('Lorem ipsum...');
+        $lesson->setVideoUrl('https://youtu.be/BFzeAGvLvBw?si=_biqXykVZ4M-Cix0');
+
+        $manager->persist($lesson);
+
+        /*
+         * ============================================================
+         * SAVE
+         * ============================================================
+         */
 
         $manager->flush();
     }
